7192b97691fe5138d5e9d7e80c5068b1f8210c31
wx10173734 <1179382172@qq.com>
2022-07-18 16:33:04 +0800

    2022.7.18 下午 完成登录功能(配置拦截器)以及员工列表分页展示

--- src/main/java/com/lzc/ReggieTakeOutApplication.java
+++ src/main/java/com/lzc/ReggieTakeOutApplication.java
@@ -3,8 +3,11 @@ package com.lzc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieTakeOutApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieTakeOutApplication.class, args);
---
+++ src/main/java/com/lzc/config/MyBatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.lzc.config;
+/**
+ * @author luozouchen
+ * @date 2022/7/18
+ * @apiNote
+ */
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置mp分页插件
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
