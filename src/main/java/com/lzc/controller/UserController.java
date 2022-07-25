678388b4d3c03a3825cac70bb3405fb4063b7efe
wx10173734 <1179382172@qq.com>
2022-07-25 23:14:10 +0800

    2022.7.25 晚上完成redis缓存登录验证码

--- src/main/java/com/lzc/controller/UserController.java
+++ src/main/java/com/lzc/controller/UserController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author luozouchen
@@ -29,6 +31,8 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 发送手机短信验证码
@@ -40,8 +44,7 @@ public class UserController {
     public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         //获取手机号
         String phone = user.getPhone();
-
-        if (StringUtils.isNotEmpty(phone)){
+        if (StringUtils.isNotEmpty(phone)) {
             //生成随机4位验证码
             //生成随机的4位验证码
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
@@ -49,46 +52,50 @@ public class UserController {
             //调用腾讯云提供的短信服务api完成发送短信
             //SendSms.sendMessage(phone,code);
             //需要将生成的验证码保存到session
-            session.setAttribute(phone,code);
+            //session.setAttribute(phone,code);
+            //需要将生成的验证码缓存到redis中，并且设置有效期位5分钟
+            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
             return R.success("验证码发送成功");
-
         }
         return R.error("验证码发送失败");
     }
 
     /**
      * 移动端用户登录
+     *
      * @param map
      * @param session
      * @return
      */
     @PostMapping("/login")
-    public R<User> login(@RequestBody Map map, HttpSession session){
+    public R<User> login(@RequestBody Map map, HttpSession session) {
         log.info((String) map.get("code"));
         //获取手机号
         String phone = (String) map.get("phone");
         //获取验证码
-        String code =(String)map.get("code");
+        String code = (String) map.get("code");
         log.info(code);
         //从session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
+        //Object codeInSession = session.getAttribute(phone);
+        //从redis中获取验证码
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
         //进行验证码的比对（页面提交的验证码和session中保存的验证码比对）
-        if (codeInSession!=null &&codeInSession.equals(code)){
+        if (codeInSession != null && codeInSession.equals(code)) {
             //如果能比对成功，说明登录成功
-
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(User::getPhone,phone);
+            queryWrapper.eq(User::getPhone, phone);
             User user = userService.getOne(queryWrapper);
-            if (user==null){
+            if (user == null) {
                 //判断当前手机号是否位新用户，如果是新用户自动完成注册
                 user = new User();
                 user.setPhone(phone);
                 user.setStatus(1);
                 userService.save(user);
             }
-            session.setAttribute("user",user.getId());
+            session.setAttribute("user", user.getId());
+            //如果用户登录成功，删除redis中缓存的验证码
+            redisTemplate.delete(phone);
             return R.success(user);
-
         }
         return R.error("登录失败");
     }
